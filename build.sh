ff8958861e73fd9256407d9ef1e1f0516e663917
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-10-11 21:52:06 -0700

    This one should do the trick

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-[ -f vendor/rabbitmq-c/librabbitmq/amqp.h ] || ./get_deps
+#!/bin/sh
+[ -f vendor/rabbitmq-c/librabbitmq/.libs/librabbitmq.a ] || ./get_deps
 [ -f Makefile.in ] || autoreconf -i
 [ -f Makefile ] || ./configure
 make
